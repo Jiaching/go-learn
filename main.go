6c8b462887438058fb893f020e2852817b4db0f6
jiachingjian <sagiters1128@gmail.com>
2024-08-11 15:14:06 +0800

    feat - 更新新的提示方式

--- main.go
+++ main.go
@@ -20,17 +20,18 @@ func main() {
 	var characterSituationAdverb string
 	var characterVerb string
 
-	fmt.Println(GetUserInputWithPromptHints("test %s", []string{"aaa", "bbb"}))
-
-	return
-
+	var jobHints = []string{"初學者", "冒險家", "流浪漢"}
+	var personalAdjectiveHints = []string{"勇者", "膽小", "堅強", "衝動", "隨便"}
+	var goalHints = []string{"勇者", "獵人", "富翁"}
+	var situationHints = []string{"幸運地", "糟糕地", "幸輕鬆地", "艱難地"}
+	var verbHints = []string{"發現", "擊殺", "收服", "催眠"}
 	fmt.Println("歡迎來到故事模板，接下來會請你輸入不同的詞彙，來透過模板創建一個故事")
 	// 隨機輸入名詞/動詞/形容詞
-	Prompt("請輸入角色現況的職業(e.g.初學者/冒險家/流浪漢)", &characterJob)
-	Prompt("請輸入角色的人格特質(e.g.勇者/膽小/堅強/衝動/隨便)", &characterPersonalAdjective)
-	Prompt("請輸角色未來想要成為的目標(e.g. 勇者/獵人/富翁)", &characterGoal)
-	Prompt("請輸入角色今日的狀態(e.g.幸運地/糟糕地/幸輕鬆地/艱難地)", &characterSituationAdverb)
-	Prompt("請輸入角色今日的動作(e.g. 發現/擊殺/收服/催眠)", &characterVerb)
+	PromptHints("請輸入角色現況的職業 %s", jobHints, &characterJob)
+	PromptHints("請輸入角色的人格特質 %s", personalAdjectiveHints, &characterPersonalAdjective)
+	PromptHints("請輸角色未來想要成為的目標 %s", goalHints, &characterGoal)
+	PromptHints("請輸入角色今日的狀態 %s", situationHints, &characterSituationAdverb)
+	PromptHints("請輸入角色今日的動作 %s", verbHints, &characterVerb)
 
 	fmt.Println("\n這是屬於您的故事")
 	story := fmt.Sprintf("從前有一位%s，個性非常的%s\n"+
@@ -51,8 +52,7 @@ func Prompt(prompt string, input *string) {
 
 // 提示範例： 請輸入XX(名詞) 或是輸入數字，使用提示的詞彙(1.aaa 2.bbbb 3.ccc)
 // 回傳玩家輸入的詞彙
-func GetUserInputWithPromptHints(prompt string, hints []string) string {
-	var userInput string
+func PromptHints(prompt string, hints []string, userInput *string) {
 	var hintsBuilder strings.Builder
 
 	// 組合提示詞
