379a47de8c9066e49ff7ee0cd5ae5d50cde463fa
jiachingjian <sagiters1128@gmail.com>
2024-08-17 22:12:05 +0800

    feat - 調整成function

--- C1_HW_Fibonacci/main.go
+++ C1_HW_Fibonacci/main.go
@@ -4,22 +4,28 @@ import "fmt"
 
 func main() {
 	var num int
-	var numOfFibonacci = []int{1, 1}
+
 	fmt.Println("請輸入要計算費式數列的數字")
 	fmt.Scanln(&num)
-	if num < 2 {
+
+	fibonacci := calculateFibonacci(num)
+	if fibonacci == nil {
 		fmt.Println("請輸入大於1的數字")
 		return
 	}
+
+	fmt.Println(fibonacci)
+}
+
+func calculateFibonacci(num int) []int {
+	if num < 2 {
+		return nil
+	}
+
+	var numOfFibonacci = []int{1, 1}
 	for index := len(numOfFibonacci); index < num; index++ {
 		numOfFibonacci = append(numOfFibonacci, numOfFibonacci[index-2]+numOfFibonacci[index-1])
 	}
 
-	fmt.Println(numOfFibonacci)
+	return numOfFibonacci
 }
-
-//func fibonacci(num int, numOfFibonacci *[]int) {
-//	for index := len(*numOfFibonacci); index < num; index++ {
-//		*numOfFibonacci= append(*numOfFibonacci, numOfFibonacci[index-2], numOfFibonacci[index-1])
-//	}
-//}
